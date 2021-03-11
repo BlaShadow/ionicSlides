d8b7e8c1eb7ba78385b2aaefe9549fe42db5cdc2
blashadow <blackzerogamer@gmail.com>
2021-03-10 21:05:35 -0400

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonImg, IonPage, IonSlide, IonSlides, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
+import image1 from './assets/astronomy-1867616_1280.jpg';
+import image2 from './assets/bird-6062768_1280.jpg';
+import image3 from './assets/kingfisher-881594_1280.jpg';
+import image4 from './assets/milky-way-984050_1280.jpg';
+
 const Home: React.FC = () => {
+  const images = [image1, image2, image3, image4]
+  const slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -13,10 +24,18 @@ const Home: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Image Slider</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonSlides pager={true} options={slideOpts}>
+          {images.map((item, index) => {
+            return (
+              <IonSlide key={index}>
+                <IonImg style={{height: 300, objectFit: 'cover'}} src={item} />
+              </IonSlide>
+            );
+          })}
+        </IonSlides>
       </IonContent>
     </IonPage>
   );
